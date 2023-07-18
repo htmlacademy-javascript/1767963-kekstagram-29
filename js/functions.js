b7798502a278687a072b03f6ab9d28036ede0da6
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2023-07-18 08:51:42 +0500

    продолжение начало проверки палиндрома

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,9 @@ const string = 'строка';
 //строка должна быть не меньше определенной длины?
 let result;
 result = (string.length <= 20) ? 'Строка нужной длины' : 'Строка неподходит';
+//палиндром
+let newString = toLowerCase(replaceAll(string));
+
+for (let i = newString.length - 1; i === 0; i = i - 1) {
+
+}
