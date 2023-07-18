69c8d83b39f3926943df31426f559acae43c4041
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2023-07-18 21:06:47 +0500

    текущее состояние палиндром

--- js/functions.js
+++ js/functions.js
@@ -9,3 +9,12 @@ let stringLength = function (string, maxSimbols) {
 
 let result = stringLength('ggg hhtht ertw', 20);
 console.log (result);
+
+//палиндром
+let polyString = function (string) {
+ let normalString = string.replaceAll(" ", "").toUpperCase();
+ return normalString;
+};
+
+let poly = polyString('Лёша на полке клопа нашёл ');
+console.log (poly);
