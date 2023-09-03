92a916170e7187160dc2a3db5c1d66631f1d257f
KomlevaAlena <113859965+KomlevaAlena@users.noreply.github.com>
2023-09-03 05:20:28 +0500

    создала и подключила файл для просмотра большого фото

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import './functions.js';
 import { createPhotos } from './data.js';
 import { renderPhotos } from'./picture.js';
+import './big-picture.js';
 
 
 const randomPhotoObjects = createPhotos();//генерирует обьекты с фотографиями
